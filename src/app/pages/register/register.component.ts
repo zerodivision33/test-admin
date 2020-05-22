5381a5725f033763f05a8ad2332d778b2e2d076e
Mohan Cm <mohanmanjappa@gmail.com>
2020-05-22 21:29:31 +0530

    Create object to register email and password

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+
+  registerUserData = {}
   constructor() { }
 
   ngOnInit() {
+  }
+
+  registerUser() {
+    console.log(this.registerUserData)
   }
 
 }
